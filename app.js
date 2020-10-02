f11bf7542ecf170d5679c32136f504f5efd60d77
Nina Loeterman <nina.loeterman@gmail.com>
2020-10-02 15:13:17 +0300

    successfully sending emails

--- app.js
+++ app.js
@@ -1,16 +1,52 @@
-const express = require('express');
+const express = require("express");
+const nodemailer = require("nodemailer");
+const path = require("path");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 
+const port = 5050;
 
-const port = 5050
+app.use(cors());
 
-app.use(cors())
+app.get("/", function (req, res) {
+  res.send("hello world!");
+  main();
+});
 
-app.get('/', function (req, res) {
-    res.send('Hello World!')
-  })
+app.post("/form", function (req, res) {
+  const formData = req.query;
+  res.send(formData);
+  main(formData).catch(console.error);
+});
 
-  app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-  })
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
+
+async function main(formData) {
+  var transporter = nodemailer.createTransport({
+    service: "outlook",
+    port: 25,
+    auth: {
+      user: "meitar.takeaway@outlook.com",
+      pass: "takemeaway!!!",
+    },
+  });
+
+  var mailOptions = {
+    from: "meitar.takeaway@outlook.com",
+    to: "nina.loeterman@gmail.com",
+    subject: "An order was placed!",
+    // text: formData.course,
+    html: `<div>couple: ${formData.couple}</div>
+           <div>first course: ${formData.firstCourse}</div>`,
+  };
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log("Email sent: " + info.response);
+    }
+  });
+}
