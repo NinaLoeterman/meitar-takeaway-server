bd1fff8ac1bd6d08788c7f8927388177c0fd8b4a
Nina Loeterman <nina.loeterman@gmail.com>
2020-10-03 23:05:53 +0300

    posting form data functional

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const nodemailer = require("nodemailer");
-const path = require("path");
 const app = express();
 const cors = require("cors");
 
@@ -8,13 +7,17 @@ const port = 5050;
 
 app.use(cors());
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", function (req, res) {
   res.send("hello world!");
   main();
 });
 
-app.post("/form", function (req, res) {
-  const formData = req.query;
+app.post("/data", (req, res) => {
+  const formData = req.body.data;
+  console.log(formData);
   res.send(formData);
   sendOrderEmail(formData).catch(console.error);
 });
@@ -24,6 +27,7 @@ app.listen(port, () => {
 });
 
 async function sendOrderEmail(formData) {
+  console.log('inside send order', formData)
   var transporter = nodemailer.createTransport({
     service: "outlook",
     port: 25,
@@ -37,8 +41,11 @@ async function sendOrderEmail(formData) {
     from: "meitar.takeaway@outlook.com",
     to: "nina.loeterman@gmail.com",
     subject: "An order was placed!",
-    html: `<div>couple: ${formData.couple}</div>
-           <div>first course: ${formData.firstCourse}</div>`,
+    html: `<div>Full Name: ${formData.fullName}</div>
+           <div>Phone Number: ${formData.mobileNumber}</div>
+           <div>First Course: ${formData.firstCourse}</div>
+           <div>Second Course: ${formData.secondCourse}</div>
+           <div>Third Course: ${formData.thirdCourse}</div>`,
   };
 
   transporter.sendMail(mailOptions, (error, info) => {
